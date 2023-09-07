0e1b4a88417af47d91e7da33ce844c946282911b
Guillaume Lucazeau <glucazeau@gmail.com>
2023-09-07 22:41:07 +0200

    Add persistence with SQLAlchemy

--- api/dish.py
+++ api/dish.py
@@ -1,15 +1,39 @@
-from dataclasses import dataclass
+from pydantic import BaseModel
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import MappedAsDataclass, Session, DeclarativeBase
 
 
-@dataclass
-class Dish:
+
+class Base(MappedAsDataclass, DeclarativeBase):
+    pass
+
+class DishModel(BaseModel):
     name: str
 
 
-def list_dishes():
-    return [
-        Dish("Pâtes carbonara"),
-        Dish("Spaghetti bolognaise"),
-        Dish("Sushi"),
-        Dish("Pizza"),
-    ]
+class DishEntity(Base):
+    __tablename__ = "menus"
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    name: Mapped[str]
+
+
+#def list_dishes():
+    #return [
+    #    DishEntity(name="Pâtes carbonara"),
+    #    DishEntity(name="Spaghetti bolognaise"),
+    #    DishEntity(name="Sushi"),
+    #    DishEntity(name="Pizza"),
+#]
+
+
+def list_dishes(db: Session):
+    return db.query(DishEntity)
+
+
+def create_dish(db: Session, dish: DishModel):
+    new_dish = DishEntity(name=dish.name)
+    db.add(new_dish)
+    db.commit()
+    db.refresh(new_dish)
+    return new_dish
--- api/main.py
+++ api/main.py
@@ -1,16 +1,38 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+from typing import List
 
-from dish import list_dishes
-from season import list_seasons
+import dish
+import season
+
+from database import SessionLocal, engine
+
+
+dish.Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
 
 
-@app.get("/dishes")
-async def get_dishes():
-    return list_dishes()
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/dish/", response_model=dish.DishEntity)
+def create_dish(new_dish: dish.DishModel, db: Session = Depends(get_db)):
+    return dish.create_dish(db=db, dish=new_dish)
+
+
+@app.get("/dishes", response_model=List[dish.DishModel])
+async def get_dishes(db: Session = Depends(get_db)):
+    return dish.list_dishes(db)
 
 
 @app.get("/seasons")
 async def seasons():
-    return list_seasons()
+    return season.list_seasons()
