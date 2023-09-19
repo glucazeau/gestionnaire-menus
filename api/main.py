1a6759920cb07406c5bb0e3f9b47567062eac49c
Guillaume Lucazeau <glucazeau@advanced-schema.com>
2023-09-19 07:41:51 +0200

    Add button and endpoint to regenerate a week

--- api/main.py
+++ api/main.py
@@ -57,5 +57,5 @@ async def get_weeks(db: Session = Depends(get_db)):
 
 
 @app.get("/week/{year}/{number}", response_model=schemas.Week)
-async def get_week(year, number, db: Session = Depends(get_db)):
-    return week.get_week_menus(db, year, number, True)
+async def get_week(year, number, db: Session = Depends(get_db), generate: bool = False):
+    return week.get_week_menus(db, year, number, generate)
